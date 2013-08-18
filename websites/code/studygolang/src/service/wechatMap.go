db808b664986e8eb745cf18256be10db4b1a0ec2
xuzhiping7 <xuzhiping7@qq.com>
2013-08-19 04:25:15 +0800

    update

--- websites/code/studygolang/src/service/wechatMap.go
+++ websites/code/studygolang/src/service/wechatMap.go
@@ -11,7 +11,7 @@ var Map_MapData map[int]*model.WechatMap
 func init() {
 	Map_MapData = make(map[int]*model.WechatMap)
 	Map_MapData[0] = model.NewWechatMap(0, "林风角酒馆", "吵杂的林风村的小酒馆，你可以向酒侍询问消息，可以跟玩家组队。", []int{}, []int{}, []int{}, []int{})
-	Map_MapData[1] = model.NewWechatMap(1, "林风村", "林风角的一个小村落，常年大风，在海角之上临海而拔起，空气潮湿。春秋之季常遭海怪袭击。", []int{}, []int{}, []int{2, 3}, []int{1})
+	Map_MapData[1] = model.NewWechatMap(1, "林风村", "林风角的一个小村落，常年大风，在海角之上临海而拔起，空气潮湿。春秋之季常遭海怪袭击。\n\n'买'：购买物品\n'卖'：出售物品", []int{}, []int{}, []int{2, 3}, []int{1})
 	Map_MapData[2] = model.NewWechatMap(2, "林风南海岸", "海怪的聚集地，是个'修炼'的好地方。", []int{0, 1}, []int{50, 50}, []int{1}, []int{})
 
 	//logger.Debugln(map_MapData["2"])
